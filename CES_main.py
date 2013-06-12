29e3f431e3124715742cbcbd1ddf4ba34b2c6fcb
Markus Koskinen <markus.koskinen@futurice.com>
2013-06-12 17:46:51 +0300

    logging updates, optparser global ugliness, google-service rename

--- CES_main.py
+++ CES_main.py
@@ -15,43 +15,48 @@ import sys
 
 import CES_db
 import CES_calendar
-import CES_googleservices
+import CES_googleservice
 import CES_group
 
 from CES_util import *
 from local_settings import *
 from optparse import OptionParser
 
-OPTIONS = {}
+global CMD_OPTIONS
+parser = OptionParser(usage="Usage: %prog [options]")
+parser.add_option("-s", "--simulate",
+                  action="store_true",
+                  dest="simulate_only",
+                  default=False,
+                  help="Simulate only. Do not do any writes or updates.")
+    
+CMD_OPTIONS, _ = parser.parse_args()
+
+import testing
 
 def main(argv):
     init_logging()
 
-    parser = OptionParser(usage="Usage: %prog [options]")
-    parser.add_option("-s", "--simulate",
-                      action="store_true",
-                      dest="simulate_only",
-                      default=False,
-                      help="Simulate only. Do not do any writes or updates.")
-    
-    (OPTIONS, _) = parser.parse_args()
-
-    if OPTIONS.simulate_only:
+    if CMD_OPTIONS.simulate_only:
         logging.info("Simulate switch is enabled. Will not do writes.")
 
     CES_db.init_db()
-    calendar_service, directory_service = CES_googleservices.init_googleservices()
+    calendar_service, directory_service = CES_googleservice.init_googleservices()
 
     logging.info("Populating list of all groups ...")
     CES_group.ALL_GROUPS = CES_group.get_groups(directory_service)
+    logging.info("Done. Sizeof all groups: %s" % len(CES_group.ALL_GROUPS))
+    logging.debug("All groups: %s" % CES_group.ALL_GROUPS)
 
     logging.info("Fetching all master events ...")
     master_events = CES_calendar.get_master_events(calendar_service)
+    logging.info("Done. Sizeof all master events: %d" % len(master_events))
 
     logging.debug("Master events:\n%s" % pp.pformat(master_events))
 
     logging.info("Creating CES events ...")
     ces_events = [CES_calendar.cesEvent(mevent) for mevent in master_events if not "recurringEventId" in mevent]
+    logging.info("Done. Sizeof CES events: %d" % len(ces_events))
 
     logging.info("Applying CES events to calendars ...")
     for ces_event in ces_events:
